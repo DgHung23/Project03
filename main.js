29e845221da3680380c9120dcee469c39a020b16
DgHung23 <hungdangmcn@gmail.com>
2023-06-28 23:31:05 +0700

    add main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+isAccept = confirm('confirm accept');
+if (isAccept){
+    alert("welcome to my website")
+} else {
+    for (var i = 0; i <1000; i++) {
+        alert("you can not accept")
+    }
+}
